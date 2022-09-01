31985fcef264f5b1875e7e9a96c9cab736e56f9c
thomaswening <thomaswening94@gmail.com>
2022-09-02 00:27:24 +0200

    Add project files.

---
+++ AttachedPropertiesDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AttachedPropertiesDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtName.Text = string.Empty;
+            txtName.SetValue(TextBoxExtension.WasUsedProperty, true); // Can this be made neater with the custom setter?
+            TextBoxExtension.SetWasUsed(txtName, true); // Yes, it can!
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtName.Text) && (bool)txtName.GetValue(TextBoxExtension.WasUsedProperty)) // Can't we use the custom getter?
+            {
+                txtMessage.Text = $"Your name is {txtName.Text}!";
+                txtMessage.Text += $" {TextBoxExtension.GetWasUsed(txtName)}"; // The hell it can!
+            }
+        }
+    }
+}
---
+++ AttachedPropertiesDemo/TextBoxExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AttachedPropertiesDemo
+{
+    public class TextBoxExtension : TextBox // Create a class that inherits from the control to which you want to attach a property
+    {
+        // Register an attached dependency property for the control as a backing field
+        // The dependency property must be named {identfier}Property.
+
+        public static readonly DependencyProperty WasUsedProperty = DependencyProperty.RegisterAttached(
+
+                "WasUsed",                              // The actual name of the property, i.e. {identifier} above
+                typeof(bool),                           // Type of the property
+                typeof(TextBoxExtension),               // This must always be the type of this class - not TextBox, for example
+                new PropertyMetadata(false)             // You can define a default value here
+            );
+
+        // Define a getter and a setter which take the control as a parameter
+        public static bool GetWasUsed(TextBox target)
+        {
+            return (bool)target.GetValue(WasUsedProperty);
+        }
+
+        public static void SetWasUsed(TextBox target, bool value)
+        {
+            target.SetValue(WasUsedProperty, value);
+        }
+    }
+}
